ca2ebac15eb69ecc0a742da11e370aab7908badb
heoooo <jinhyeok9290@gmail.com>
2023-02-04 00:50:21 +0900

    Commit message

---
+++ posts/permissions.py
@@ -0,0 +1,12 @@
+from rest_framework import permissions
+
+class CustomReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method =='GET':
+            return True
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
